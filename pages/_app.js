0ef2a459dd723ea21591620302a3fc9db0746da3
Ubaid <shaikhubaid769@gmail.com>
2022-09-26 04:56:00 +0530

    Add Google Analytics (Universal Analytics)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import 'remixicon/fonts/remixicon.css'
 
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import Layout from '../components/Layout'
 
+const TRACKING_ID = "UA-242733733-1";
+ReactGA.initialize(TRACKING_ID);
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return <Layout><Component {...pageProps} /></Layout>
 }
 
