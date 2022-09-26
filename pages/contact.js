73cc10055ed0bf51e59e3b321886dae278270a05
Ubaid <shaikhubaid769@gmail.com>
2022-09-26 05:48:21 +0530

    Add Contact Form UI

--- pages/contact.js
+++ pages/contact.js
@@ -1,77 +1,70 @@
 function contact() {
     return (
         <div className="flex items-center justify-center p-12">
-            <div className="mx-auto w-full max-w-[550px]">
-                {/* <form action="https://formbold.com/s/FORM_ID" method="POST">
-                    <div className="mb-5">
-                        <label
-                            for="name"
-                            className="mb-3 block text-base font-medium"
-                        >
-                            Full Name
-                        </label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            placeholder="Full Name"
-                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
-                        />
-                    </div>
-                    <div className="mb-5">
-                        <label
-                            for="email"
-                            className="mb-3 block text-base font-medium"
-                        >
-                            Email Address
-                        </label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            placeholder="example@domain.com"
-                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
-                        />
-                    </div>
-                    <div className="mb-5">
-                        <label
-                            for="subject"
-                            className="mb-3 block text-base font-medium"
-                        >
-                            Subject
-                        </label>
-                        <input
-                            type="text"
-                            name="subject"
-                            id="subject"
-                            placeholder="Enter your subject"
-                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
-                        />
-                    </div>
-                    <div className="mb-5">
-                        <label
-                            for="message"
-                            className="mb-3 block text-base font-medium"
-                        >
-                            Message
-                        </label>
-                        <textarea
-                            rows="4"
-                            name="message"
-                            id="message"
-                            placeholder="Type your message"
-                            className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
-                        ></textarea>
-                    </div>
-                    <div>
-                        <button
-                            className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </form> */}
-                Email: nanobet769@gmail.com
+            <div>
+                <article className="prose prose-lg mb-5">
+                    <h1>Contact</h1>
+                    <p>
+                        If there is anything you would want to discuss with me,
+                        get in touch!
+                    </p>
+                </article>
+                <div className="mx-auto w-full max-w-[550px]">
+                    <form action="https://formbold.com/s/FORM_ID" method="POST">
+                        <div className="mb-5">
+                            <label
+                                htmlFor="name"
+                                className="mb-3 block text-base font-medium"
+                            >
+                                Name:
+                            </label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Name"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
+                            />
+                        </div>
+                        <div className="mb-5">
+                            <label
+                                htmlFor="email"
+                                className="mb-3 block text-base font-medium"
+                            >
+                                Email:
+                            </label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="example@domain.com"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
+                            />
+                        </div>
+                        <div className="mb-5">
+                            <label
+                                htmlFor="message"
+                                className="mb-3 block text-base font-medium"
+                            >
+                                Message:
+                            </label>
+                            <textarea
+                                rows="4"
+                                name="message"
+                                id="message"
+                                placeholder="Type your message"
+                                className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium outline-none focus:border-[#6A64F1] focus:shadow-md"
+                            ></textarea>
+                        </div>
+                        <div>
+                            <button
+                                className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
+                            >
+                                Send
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
