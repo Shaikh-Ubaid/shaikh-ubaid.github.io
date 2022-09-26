a0d06241dd57e7f5497868a28f8e142335e80a32
Ubaid <shaikhubaid769@gmail.com>
2022-09-26 06:50:13 +0530

    1. Try to fix Google Analytics
    2. Add MEASUREMENT_ID in code

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,13 @@ import Script from "next/script";
 import HeadMeta from "./HeadMeta";
 import Navbar from "./Navbar";
 
+const GA_MEASUREMENT_ID = "G-LM46T5SBKY";
+
 function Layout({ children }) {
     return (
         <div>
             <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
                 strategy="afterInteractive"
             />
             <Script id="google-analytics" strategy="afterInteractive">
@@ -16,7 +18,7 @@ function Layout({ children }) {
                     function gtag(){window.dataLayer.push(arguments);}
                     gtag('js', new Date());
 
-                    gtag('config', ${process.env.GA_MEASUREMENT_ID});
+                    gtag('config', ${GA_MEASUREMENT_ID});
                 `}
             </Script>
             <HeadMeta />
