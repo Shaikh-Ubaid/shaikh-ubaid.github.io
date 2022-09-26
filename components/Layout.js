75d77de236570ffa4a06d8cf88b7891372162f36
Ubaid <shaikhubaid769@gmail.com>
2022-09-26 05:53:58 +0530

    Use Google Analytics in NextJS inplace of react-ga

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,24 @@
+import Script from "next/script";
+
 import HeadMeta from "./HeadMeta";
 import Navbar from "./Navbar";
-
 
 function Layout({ children }) {
     return (
         <div>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', ${process.env.GA_MEASUREMENT_ID});
+                `}
+            </Script>
             <HeadMeta />
             <Navbar />
             {children}
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import '../styles/globals.css'
 import 'remixicon/fonts/remixicon.css'
 
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
 import Layout from '../components/Layout'
 
-const TRACKING_ID = "UA-242733733-1";
-ReactGA.initialize(TRACKING_ID);
-
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return <Layout><Component {...pageProps} /></Layout>
 }
 
